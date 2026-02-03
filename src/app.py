517a2dd278f220a3f87d8e57badf2e58b71b61a6
Brenda-Machado <araucaria2011@gmail.com>
2026-02-03 14:42:29 -0300

    app that creates conexion with database

--- src/app.py
+++ src/app.py
@@ -10,13 +10,40 @@ from flask import Flask, render_template, request, redirect, url_for, g
 import os
 import sqlite3
 
-APP = Flask(__name__)
+app = Flask(__name__)
 DATABASE = os.path.join(APP.instance_path, "library.db")
 
 # Database Funcitions
 
 def get_database():
-    if "db" not in g: #  simple namespace object that has the same lifetime as an application context.
+    if "db" not in g: # simple namespace object that has the same lifetime as an application context.
         g.db = sqlite3.connect(DATABASE)
         g.db.row_factory = sqlite3.Row
         g.db.execute("PRAGMA foreign_keys = ON")
+    return g.db
+
+def init_database():
+    db = get_database()
+    db.executescript("""CREATE TABLE IF NOT EXISTS books (
+            isbn        TEXT PRIMARY KEY,
+            title       TEXT NOT NULL,
+            author      TEXT NOT NULL,
+            publisher   TEXT,
+            year        INTEGER,
+            genre       TEXT,
+            language    TEXT,
+            pages       INTEGER,
+            date_read   TEXT,
+            rating      INTEGER CHECK (rating BETWEEN 1 AND 5),
+            review      TEXT
+        ); """)
+
+@app.cli.command("init-database")
+def init_database_command():
+    init_database()
+
+@app.teardown_appcontext
+def close_database(exception):
+    db = g.pop("db", None) # closes the conexion at the end of requisition
+    if db is not None:
+        db.close()
